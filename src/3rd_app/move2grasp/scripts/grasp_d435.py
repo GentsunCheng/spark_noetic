53cbfbce03845b9342a64a05c38495578ab33378
spark <orii@duck.com>
2024-07-04 14:12:47 +0800

    bug fixed

--- src/3rd_app/move2grasp/scripts/grasp_d435.py
+++ src/3rd_app/move2grasp/scripts/grasp_d435.py
@@ -228,32 +228,31 @@ class GraspObject():
 
     # 执行抓取
     def grasp(self):
-        rospy.loginfo("start to grasp\n")
+        rospy.loginfo("start to grasp")
         # stop function
 
         filename = os.environ['HOME'] + "/thefile.txt"
         file_pix = open(filename, 'r')
         s = file_pix.read()
         file_pix.close()
-        rospy.loginfo(s)
         arr = s.split()
         a1, a2, a3, a4 = arr[0], arr[1], arr[2], arr[3]
-        a, b = [0]*2, [0]*2
+        a, b = [0] * 2, [0] * 2
         a[0], a[1], b[0], b[1] = float(a1), float(a2), float(a3), float(a4)
-        rospy.loginfo('k and b value:', a[0], a[1], b[0], b[1])
+        rospy.loginfo(f"k and b value: {a[0]} {a[1]} {b[0]} {b[1]}")
         r2 = rospy.Rate(10)
         pos = position()
         # 物体所在坐标+标定误差
         pos.x = a[0] * self.yc_prev + a[1]
         pos.y = b[0] * self.xc_prev + b[1]
         pos.z = -25.0
-        rospy.loginfo("z = -25\n")
+        rospy.loginfo("z = -25")
         self.pub1.publish(pos)
         r2.sleep()
 
         pos.z = -50.0
         self.pub1.publish(pos)
-        rospy.loginfo("z = -50\n")
+        rospy.loginfo("z = -50")
 
         # 开始吸取物体
         self.pub2.publish(1)
