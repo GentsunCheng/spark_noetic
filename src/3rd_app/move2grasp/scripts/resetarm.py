6687c53b753dea20eda4ea087a3229cb44ebfcb2
spark <orii@duck.com>
2024-07-12 19:47:15 +0800

    update

--- src/3rd_app/move2grasp/scripts/resetarm.py
+++ src/3rd_app/move2grasp/scripts/resetarm.py
@@ -6,16 +6,19 @@ from std_msgs.msg import String
 from swiftpro.msg import *
 from uarm import SwiftAPI
 
-
-def resetarm(_):
-    swift = SwiftAPI("/dev/ttyACM0")
-    swift.reset(110.0, 0.0, 35.0)
+class Rester():
+    def __init__(self, port="/dev/ttyACM0"):
+        self.swift = SwiftAPI(port)
+        
+    def resetarm(self):
+        self.swift.reset(110.0, 0.0, 35.0)
 
 
 if __name__ == "__main__":
+    rester = Rester()
     try:
         rospy.init_node('armreset', anonymous=False)
-        sub = rospy.Subscriber('/armreset', String, resetarm, queue_size=1)
+        sub = rospy.Subscriber('/armreset', String, rester.resetarm, queue_size=1)
         rospy.spin()
     except rospy.ROSInterruptException:
         rospy.loginfo("program interrupted before completion")
