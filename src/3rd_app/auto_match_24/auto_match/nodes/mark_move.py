d5b1ac35e372ad7f3eef0ec13c79b52e50e60d2b
spark <orii@duck.com>
2024-07-14 21:14:17 +0800

    改回pkl

--- src/3rd_app/auto_match_24/auto_match/nodes/mark_move.py
+++ src/3rd_app/auto_match_24/auto_match/nodes/mark_move.py
@@ -11,8 +11,7 @@ import tf
 import tf.transformations as tf_transformations
 import rospkg
 import os
-import json
-import numpy as np
+import pickle
 
 class MarkNav():
     def __init__(self):
@@ -26,43 +25,38 @@ class MarkNav():
         # 订阅标记事件
         rospy.Subscriber('mark_nav', String, self.mark_nav)
         # 定义标记地点字典
-        self.mark_map = {}
+        self.dict_mark = {}
         # 监听TF坐标
         self.listener = tf.TransformListener()
         rospy.sleep(1) # need to delay
     
-
-    def mark_nav(self, mark_name):
+    def mark_nav(self,mark_name):
         '''
         设定标定地点，与后面导航相关，采用字典的方式存放
         建议标记地点名字,分类区、分拣区、收取区
         '''
 
         rospack = rospkg.RosPack() 
-        mark_map_path = os.path.join(rospack.get_path('auto_match'), 'config', 'mark_map.json')  # 标记地图json文件地址
+        mark_map_path = os.path.join(rospack.get_path('auto_match'),'config', 'mark_map.pkl')  # 标记地图yaml文件地址
 
         # 转化成字符型变量
         mark_name = str(mark_name)
-        
         # 结束地点标记
-        if str.find(mark_name, str("finish")) != -1:
-            # 将字典中的 numpy 数组转换为列表
-            mark_map_to_save = {k: v.tolist() for k, v in self.mark_map.items()}
-            # 将字典信息写入 json 文件
-            with open(mark_map_path, 'w') as file:
-                json.dump(mark_map_to_save, file)
-            print("已保存json文件,文件保存在:", mark_map_path)
-            print("当前字典内容：\n", self.mark_map)
+        if str.find(mark_name,str("finish"))!= -1:
+            # 将字典信息写入 pkl 文件
+            with open(mark_map_path, 'wb') as file:
+                pickle.dump(self.dict_mark, file)
+            print("已保存pkl文件,文件保存在:",mark_map_path)
+            print("当前字典内容：\n",self.dict_mark)
+
 
         # 前往地点    
-        if str.find(mark_name, str("go")) != -1:
+        if str.find(mark_name,str("go"))!= -1:
             # 从文件中加载数据
-            with open(mark_map_path, 'r') as file:
-                mark_map_loaded = json.load(file)
-            # 将加载的数据转换回 numpy 数组
-            self.mark_map = {k: np.array(v) for k, v in mark_map_loaded.items()}
+            with open(mark_map_path, 'rb') as file:
+                self.mark_map = pickle.load(file)
             # 输出加载的数据
-            print("mark_dict=", self.mark_map)
+            print("mark_dict=",self.mark_map)
             # 提取mark名称
             mark_name = (mark_name.split())[2]
             # 判断地点位置是否在地点字典中
@@ -71,15 +65,23 @@ class MarkNav():
             else:
                 print("此地点尚未登陆在字典中")
 
+
         # 学习地点
-        if str.find(mark_name, str("learn")) != -1:
+        if str.find(mark_name,str("learn"))!= -1:
             # 提取mark名称
             mark_name = (mark_name.split())[2]
             # 获取当前位置
-            self.current_position = self.get_currect_pose()
+            self.currrent_position = self.get_currect_pose()
             # 标记当前位置
-            self.mark_map[mark_name] = self.current_position
-            print("当前字典内容：\n", self.mark_map)
+            self.dict_mark[mark_name] = self.currrent_position
+            print("当前字典内容：\n",self.dict_mark)
+
+    # 获取当前位置
+    def get_currect_pose(self):
+        (target_trans, target_rot) = self.listener.lookupTransform("map", "base_footprint", rospy.Time(0))
+        print("target_trans:",target_trans)
+        print("target_rot:",target_rot)
+        return tf_transformations.compose_matrix(translate=target_trans, angles=tf_transformations.euler_from_quaternion(target_rot))
 
     def navigation(self,mark_name):
         '''
